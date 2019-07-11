e856cb2cbf109569af014126b83e9b3b8c80ea2b
Alexandre CONTASSOT <alexandre.contassot@thechatbotfactory.com>
2019-07-11 15:49:59 +0200

    Add a sh script to generate all sls yaml

---
+++ generate-sls.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+MODULE_="$1"
+
+err_() {
+    echo -e "\033[41m\033[90mError on module $1\033[0m"
+    exit 1
+}
+
+trap 'err_ ${MODULE_}' ERR
+
+
+generate_() {
+
+  trap 'err_ $1' ERR
+
+  echo -e "\033[35mGenerate sls for $1\033[0m"
+  cd $1
+  yarn run generate-sls
+  cd ..
+}
+
+generate_all_() {
+  pathList=$(ls -d */ | grep -v "node_modules" | sed "s/\///g")
+  lambdas=(${pathList///\n/ })
+
+  for i in "${lambdas[@]}"
+  do
+      generate_ ${i}
+  done
+}
+
+if [[ -z ${MODULE_} ]]
+then
+  generate_all_
+else
+  generate_ ${MODULE_}
+fi
